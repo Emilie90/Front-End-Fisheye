ff8cbecedd9ea097dec6121cae3bb0ccb3e212f1
Emilie90 <emilie.voiron@get-mail.eu>
2023-10-20 11:38:12 +0200

    modification

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -19,29 +19,3 @@ class App {
 }
 const app = new App();
 app.main();
-
-// async function getPhotographers() {
-//   const reponse = await fetch("./data/photographers.json");
-//   const photographers = await reponse.json();
-
-//   // et bien retourner le tableau photographers seulement une fois récupéré
-//   return console.log(photographers.photographers);
-// }
-
-// async function displayData(photographers) {
-//   const photographersSection = document.querySelector(".photographer_section");
-
-//   photographers.forEach((photographer) => {
-//     const photographerModel = photographerTemplate(photographer);
-//     const userCardDOM = photographerModel.getUserCardDOM();
-//     photographersSection.appendChild(userCardDOM);
-//   });
-// }
-
-// async function init() {
-//   // Récupère les datas des photographes
-//   const { photographers } = await getPhotographers();
-//   displayData(photographers.photographers);
-// }
-
-// init();
